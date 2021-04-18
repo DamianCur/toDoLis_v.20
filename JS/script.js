ae0a17e3551a8d041806a0b120482b70fd048977
Damian Cur <damian.cur91@gmail.com>
2021-04-18 13:18:51 +0200

    added main functions

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+let $toDoInput;
+
+const main = () => {
+    prepereDOMElements();
+    prepereDOMEvents();
+}
+
+const prepereDOMElements = () => {
+    $toDoInput = document.querySelector(".toDoInput");
+}
+
+const prepereDOMEvents = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", main)
